58068a175720b658d7ec82118754ab795b09b4f4
Kevin Østerkilde <kevin@oesterkilde.dk>
2016-07-20 09:21:08 +0200

    Updated SCSS

--- src/js/sections/random.js
+++ src/js/sections/random.js
@@ -4,7 +4,7 @@ class Random extends React.Component {
   render() {
     return (
       <section id="random">
-        <div className="container text--center">
+        <div className="container chamber-triple--ends text--center">
           <p>Blog tousled man bun bitters fap, retro letterpress. Try-hard truffaut raw denim brunch, plaid dreamcatcher migas messenger bag fanny pack godard man braid. YOLO distillery cardigan, brunch chia swag selfies post-ironic ennui 90's biodiesel drinking vinegar +1 small batch. Fixie kale chips leggings mustache post-ironic biodiesel, artisan tattooed.</p>
         </div>
       </section>
