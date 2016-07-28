620224d7a8196ac89ad2899c7939320ea6bd3add
Kevin Østerkilde <kevin@oesterkilde.dk>
2016-07-28 22:07:10 +0200

    Small changes

--- src/js/hero.js
+++ src/js/hero.js
@@ -6,9 +6,9 @@ class Hero extends React.Component {
     return (
       <section id="hero">
         <div className="container text--center chamber--sides">
-          <h1 className="font-white">My name is {name}, and I create awesome stuff!</h1>
-          <p>Blog tousled man bun bitters fap, retro letterpress. Try-hard truffaut raw denim brunch, plaid dreamcatcher migas messenger bag fanny pack godard man braid. YOLO distillery cardigan, brunch chia swag selfies post-ironic ennui 90's biodiesel drinking vinegar +1 small batch. Fixie kale chips leggings mustache post-ironic biodiesel, artisan tattooed.</p>
-          <a href={ctaUrl} className="cta push--down">{ctaName}</a>
+          <h1 className="typography--alpha font-white">My name is {name}, and I create awesome stuff!</h1>
+          <p className="typography--regular">Blog tousled man bun bitters fap, retro letterpress. Try-hard truffaut raw denim brunch, plaid dreamcatcher migas messenger bag fanny pack godard man braid. YOLO distillery cardigan, brunch chia swag selfies post-ironic ennui 90's biodiesel drinking vinegar +1 small batch. Fixie kale chips leggings mustache post-ironic biodiesel, artisan tattooed.</p>
+          <a href={ctaUrl} className="typography--regular cta push--down">{ctaName}</a>
         </div>
       </section>
     )
