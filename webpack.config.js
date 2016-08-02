ccd47345991a628afb619e88e0096ff4390dd3ac
Kevin Østerkilde <kevin@oesterkilde.dk>
2016-08-02 09:27:02 +0200

    Webpack update

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const debug = process.env.NODE_ENV !== 'production';
 const webpack = require('webpack');
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const NpmInstallPlugin = require('npm-install-webpack-plugin');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const autoprefixer = require('autoprefixer');
 const precss = require('precss');
@@ -27,7 +28,7 @@ module.exports = {
     loaders: [
       {
         test: /\.jsx?$/,
-        exclude: /node_modules/,
+        exclude: /(node_modules|bower_components)/,
         loaders: [
           'babel-loader?presets[]=es2015,presets[]=stage-0,presets[]=react,plugins[]=react-html-attrs,plugins[]=transform-class-properties,plugins[]=transform-decorators-legacy',
         ],
@@ -35,8 +36,7 @@ module.exports = {
       {
         test: /\.(scss|sass)$/,
         loader: ExtractTextPlugin.extract(
-          'style',
-          'css!postcss!sass'
+          'style', 'css!postcss!sass'
         ),
       },
     ],
@@ -45,6 +45,9 @@ module.exports = {
     return [precss, autoprefixer];
   },
   plugins: debug ? [
+    new NpmInstallPlugin({
+      save: true, // --save
+    }),
     new ExtractTextPlugin('[name].css'),
     //new HtmlWebpackPlugin({
     //  title: 'Webpack demo',
