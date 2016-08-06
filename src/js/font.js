dc8731fd171286c7eb9a71bf7eda40c351c40b13
Kevin Østerkilde <kevin@oesterkilde.dk>
2016-08-06 21:26:44 +0200

    Complete temporary(?) redesign

--- src/js/font.js
+++ src/js/font.js
@@ -1,75 +1,60 @@
 import React from 'react';
 
 const Font = () =>
-  <div>
-    <section id="hero">
-      <div className="container text--center chamber--sides">
-        <h1 className="typography--alpha font-white">My name is Kevin Østerkilde, and I create awesome stuff!</h1>
-        <p className="typography--regular">Blog tousled man bun bitters fap, retro letterpress. Try-hard truffaut raw denim brunch, plaid dreamcatcher migas messenger bag fanny pack. Godard man braid YOLO distillery cardigan, brunch chia swag selfies post-ironic ennui 90's biodiesel drinking vinegar +1 small batch. Fixie kale chips leggings mustache post-ironic biodiesel, artisan tattooed.</p>
-        <a href="#work" className="typography--regular cta push--down">Get started</a>
-      </div>
-    </section>
-    <section id="random">
-      <div className="container chamber-triple--ends text--center">
-        <p>Blog tousled man bun bitters fap, retro letterpress. Try-hard truffaut raw denim brunch, plaid dreamcatcher migas messenger bag fanny pack. Godard man braid. YOLO distillery cardigan, brunch chia swag selfies post-ironic ennui 90's biodiesel drinking vinegar +1 small batch. Fixie kale chips leggings mustache post-ironic biodiesel, artisan tattooed.</p>
-      </div>
-    </section>
-    <section className="text--center" id="work">
-      <div className="grid">
-        <a href="#1">
-          <div className="grid__item desk--one-quarter lap--one-half palm--one-whole">
-            <p>Project #1</p>
-            <img role="presentation" src="./img/three_dee/SD_02.jpg" />
-          </div>
-        </a>
-        <a href="#2">
-          <div className="grid__item desk--one-quarter lap--one-half palm--one-whole">
-            <p>Project #2</p>
-            <img role="presentation" src="./img/three_dee/SD_03.jpg" />
-          </div>
-        </a>
-        <a href="#3">
-          <div className="grid__item desk--one-quarter lap--one-half palm--one-whole">
-            <p>Project #3</p>
-            <img role="presentation" src="./img/three_dee/SD_04.jpg" />
-          </div>
-        </a>
-        <a href="#4">
-          <div className="grid__item desk--one-quarter lap--one-half palm--one-whole">
-            <p>Project #4</p>
-            <img role="presentation" src="./img/three_dee/SD_05.jpg" />
-          </div>
-        </a>
-      </div>
-      <div className="grid">
-        <a href="#4">
-          <div className="grid__item desk--one-half lap--one-whole palm--one-whole image">
-            <p>Project #5</p>
-            <img role="presentation" src="./img/three_dee/SD_01.jpg" />
-          </div>
-        </a>
-        <div className="grid__item desk--one-half lap--one-whole palm--one-whole">
-          <div className="chamber-triple--sides">
-            <p className="typography--gamma">Herringbone</p>
-            <p className="typography--regular">Been working on a very detailed wood generator, which I recently finished, so I figured I'd better use it for something. Thus the herringbone floor was created.</p>
-          </div>
+  <section id="main">
+    <div className="container">
+      <div className="hero">
+        <h1 className="headline">Kevin Østerkilde</h1>
+        <p className="subline">Designer &amp; Developer</p>
+        <hr className="seperator" />
+        <p>Currently employed at a Danish startup - <a href="#">Billetto ApS</a></p>
+        <hr className="seperator" />
+        <div className="social">
+          <h3>Find me here too</h3>
+          <ul>
+            <li>
+              <a href="#">
+                <i className="fa fa-twitter"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i className="fa fa-linkedin"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i className="fa fa-dribbble"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i className="fa fa-behance"></i>
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
       <div className="grid">
-        <div className="grid__item desk--one-half lap--one-whole palm--one-whole">
-          <div className="chamber-triple--sides">
-            <p className="typography--gamma">Herringbone</p>
-            <p className="typography--regular">Been working on a very detailed wood generator, which I recently finished, so I figured I'd better use it for something. Thus the herringbone floor was created.</p>
+        <form>
+          <div className="inline">
+            <div className="field">
+              <label htmlFor="name">Name</label>
+              <input name="name" type="text" />
+            </div>
+            <div className="field">
+              <label htmlFor="email">Email</label>
+              <input name="email" type="email" />
+            </div>
           </div>
-        </div>
-        <a href="#4">
-          <div className="grid__item desk--one-half lap--one-whole palm--one-whole image">
-            <p>Project #5</p>
-            <img role="presentation" src="./img/three_dee/SD_01.jpg" />
+          <label htmlFor="message">Message</label>
+          <div className="bigBox">
+            <textarea name="message" rows="4"></textarea>
+            <button type="submit">Send message</button>
           </div>
-        </a>
+        </form>
       </div>
-    </section>
-  </div>;
+    </div>
+  </section>;
 
 export default Font;
