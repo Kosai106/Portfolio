527ca0ce11d83fad21f00c1d5642d2716027b14f
Kevin Østerkilde <kevin@oesterkilde.dk>
2017-09-24 20:15:34 +0200

    :speech_balloon: Added fancy console.log()

--- src/js/app.js
+++ src/js/app.js
@@ -5,9 +5,17 @@ import App from './screens/index';
 
 require('../scss/app.scss');
 
+const Console = console;
+
+Console.log('\n\n');
+Console.group('%c%s', 'color: #FC3565; font-size: 24px; font-family: "Varela Round", "Roboto", sans-serif;', 'Resume for nerds:');
+Console.log('%c%s', 'color: #FFF; background-color: #FC3565; padding: 6px; font-size: 18px; font-family: "Varela Round", "Roboto", sans-serif;', 'https://oesterkilde.dk/resume.json');
+Console.groupEnd();
+Console.log('\n\n');
+
 render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-	document.getElementById('app')
+  document.getElementById('app')
 );
