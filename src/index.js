8c2612d6f7008ecc22f5be5a76758b124c160a3e
Kevin Østerkilde <kevin@oesterkilde.dk>
2019-03-18 01:04:22 +0100

    2019 refactor

--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './js/screens/index';
 
-require('./scss/app.scss');
+require('./scss/new.scss');
 
+/* eslint-disable no-console */
 console.log('\n\n');
-console.group('%c%s', 'color: #FC3565; font-size: 24px; font-family: "Varela Round", "Roboto", sans-serif;', 'Resume for nerds:');
-console.log('%c%s', 'color: #FFF; background-color: #FC3565; padding: 6px; font-size: 18px; font-family: "Varela Round", "Roboto", sans-serif;', 'https://oesterkilde.dk/resume.json');
+console.group('%c%s', 'color: #1e1efd; font-size: 24px; font-family: "Varela Round", "Roboto", sans-serif;', 'Resume for nerds:');
+console.log('%c%s', 'color: #FFF; background-color: #1e1efd; padding: 6px; font-size: 18px; font-family: "Varela Round", "Roboto", sans-serif;', 'https://oesterkilde.dk/resume.json');
 console.groupEnd();
 console.log('\n\n');
+/* eslint-enable no-console */
 
 render(
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>,
+	<main>
+		<h1>Kevin Østerkilde</h1>
+		<p>
+			I solve problems by designing and building beautiful digital experiences
+			for both mobile and the web.
+		</p>
+		<br />
+		<p>New site coming soon.</p>
+		<br />
+	</main>,
 	document.getElementById('app')
 );
